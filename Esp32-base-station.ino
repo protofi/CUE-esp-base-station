b9bc64980d191f7d16046be58ac68c3b7088cc51
Tobias Emil <tobias@protofi.dk>
2019-05-09 10:09:29 +0200

    rssi added

--- Esp32-base-station.ino
+++ Esp32-base-station.ino
@@ -5,11 +5,12 @@
 RTC_DATA_ATTR int doorbellPressedCounter = 0;
 
 unsigned long heartbeatTimestamp = 0;
-unsigned long heartbeatInterval = 20; //min
+unsigned long heartbeatInterval = 30; //min
 
 int MS_M_FACTOR = 60000;
 
 const String sensorId = "6e039d00-6825-11e9-b740-d58f0755db70";
+//const String sensorId = "74bf6730-6692-11e9-9aad-5f278de1925b";
 
 void setup() {
   // put your setup code here, to run once:
@@ -57,19 +58,22 @@ void loop() {
 
       delay(1000);
   }
+
+  long rssi = WiFi.RSSI();
   
   JsonObject heartbeatPayloadObject = heartbeatPayloadDoc.to<JsonObject>();
   heartbeatPayloadObject["id"] = sensorId;
   heartbeatPayloadObject["battery_level"] = battery_level;
-  heartbeatPayloadObject["signal_strength"] = "123";
-  heartbeatPayloadObject["notification_counter"] = String(doorbellPressedCounter);
+  heartbeatPayloadObject["signal_strength"] = rssi;
+  heartbeatPayloadObject["notification_counter"] = doorbellPressedCounter;
   
   String heartbeatPayload;
   serializeJson(heartbeatPayloadDoc, heartbeatPayload);
 
+
   if( millis() - heartbeatTimestamp > heartbeatInterval * MS_M_FACTOR )
   {
-      heartbeatTimestamp += heartbeatInterval;
+      heartbeatTimestamp += heartbeatInterval * MS_M_FACTOR;
     
       Serial.println("Sending heartbeat");
       publishTelemetry("/heartbeat", heartbeatPayload);    
