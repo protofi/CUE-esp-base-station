06cef062ea68c63f0429a98ea06260c9bf9c5dbc
Tobias Emil <tobias@protofi.dk>
2019-08-23 15:55:38 +0200

    more sleep and auth error handling

--- Esp32-base-station.ino
+++ Esp32-base-station.ino
@@ -2,25 +2,20 @@
 #include "esp32-mqtt.h"
 #include <ArduinoJson.h>
 
-RTC_DATA_ATTR int doorbellPressedCounter = 0;
+#define uS_TO_M_FACTOR 60000000  /* Conversion factor for micro seconds to seconds */
 
-unsigned long heartbeatTimestamp = 0;
-unsigned long heartbeatInterval = 20; //min
-
-int MS_M_FACTOR = 60000;
+const uint32_t TIME_TO_SLEEP = 60;  /* Time ESP32 will go to sleep (in minuts) */ 
 
 bool retryNotification = false;
-
-//const String sensorId = "6e039d00-6825-11e9-b740-d58f0755db70";
-const String sensorId = "74bf6730-6692-11e9-9aad-5f278de1925b";
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   
   setupCloudIoT();
-  
-  pinMode(GPIO_NUM_34, INPUT);
+
+  //esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_M_FACTOR); //wake up every hour
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_34,1); //1 = High, 0 = Low
 
   Serial.println("MAC: ");
   Serial.println(WiFi.macAddress());
@@ -31,12 +26,11 @@ void loop() {
   // allocate the memory for the document
   const size_t CAPACITY = JSON_OBJECT_SIZE(53);
   StaticJsonDocument<CAPACITY> notificationPayloadDoc;
-  StaticJsonDocument<CAPACITY> heartbeatPayloadDoc;
 
   String notificationPayload;
   serializeJson(notificationPayloadDoc, notificationPayload);
 
-  float battery_level = (float(analogRead(GPIO_NUM_35)) / 4095)*2*3.3*1.1;
+  //float battery_level = (float(analogRead(GPIO_NUM_35)) / 4095)*2*3.3*1.1;
   
   mqttClient->loop();
   
@@ -47,36 +41,36 @@ void loop() {
       connect();
   }
 
-  if(digitalRead(GPIO_NUM_34) == HIGH || retryNotification)
-  {
-    retryNotification = false;
-    
-    Serial.println("Sending notification");
-    publishTelemetry("/doorbell", notificationPayload);
+  retryNotification = false;
 
-    delay(700);
-    retryNotification = (lastReturnCode() != 0);
-    
-    ++doorbellPressedCounter;
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  if(wakeup_reason == ESP_SLEEP_WAKEUP_EXT0)
+  {
+      Serial.println("Sending notification");
+      publishTelemetry("/notification", notificationPayload);
+      
+      delay(1000);
+      retryNotification = (lastReturnCode() != 0);
   }
 
-  long rssi = WiFi.RSSI();
-  /*
-  JsonObject heartbeatPayloadObject = heartbeatPayloadDoc.to<JsonObject>();
-  heartbeatPayloadObject["id"] = sensorId;
-  heartbeatPayloadObject["battery_level"] = battery_level;
-  heartbeatPayloadObject["signal_strength"] = rssi;
-  heartbeatPayloadObject["notification_counter"] = doorbellPressedCounter;
+  //long rssi = WiFi.RSSI();
   
-  String heartbeatPayload;
-  serializeJson(heartbeatPayloadDoc, heartbeatPayload);
-
-  if( millis() - heartbeatTimestamp > heartbeatInterval * MS_M_FACTOR )
+  switch(wakeup_reason)
   {
-      heartbeatTimestamp += heartbeatInterval * MS_M_FACTOR;
-    
-      Serial.println("Sending heartbeat");
-      publishTelemetry("/heartbeat", heartbeatPayload);    
+      case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+      case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+      case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+      case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+      case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+      default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
   }
-  */
+
+  if(!retryNotification)
+  {
+      //Go to sleep now
+      Serial.println("Going to sleep now");
+      esp_deep_sleep_start();    
+  }
 }
